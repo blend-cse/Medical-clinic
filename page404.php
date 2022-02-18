81ec3909915c7d0092cc87d2d81379ee3fae2f96
Blend Buzuku <79057081+blend-cse@users.noreply.github.com>
2022-02-18 18:48:04 +0100

    Add files via upload

---
+++ page404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="/css/style.css"/>
+    <link rel="stylesheet" href="/vendors/font-awsome/css/all.css"/>
+
+</head>
+
+<body>
+<header>
+    <?php include "components/header.php"?>
+
+    <div class="page-title">
+        <h2 class="h1-aboutus">Error 404</h2>
+    </div>
+
+</header>
+<div class="about-section" style="text-align: center">
+   <h1>Page not found</h1>
+</div>
+
+
+<br></br>
+<footer>
+    <div class="footer">
+        <p>@ 2020 by Medical Clinic.</p>
+        <a href="https://www.facebook.com/" class="fab"><i class="fab fa-facebook-f"></i></a>
+
+    </div>
+</footer>
+</body>
+
+</html>
